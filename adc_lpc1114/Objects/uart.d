6e31372f924d20ad887fb1317e3b9c51528fdf88
tonixtom <ton.ix.tom@gmail.com>
2020-02-09 19:14:57 +0800

    again

---
+++ adc_lpc1114/Objects/uart.d
@@ -0,0 +1,8 @@
+.\objects\uart.o: CFG\uart.c
+.\objects\uart.o: .\LPC11XX\lpc11xx.h
+.\objects\uart.o: .\LPC11XX\core_cm0.h
+.\objects\uart.o: G:\softs\kile_v5_S\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\uart.o: .\LPC11XX\core_cmInstr.h
+.\objects\uart.o: .\LPC11XX\core_cmFunc.h
+.\objects\uart.o: .\LPC11XX\system_LPC11xx.h
+.\objects\uart.o: CFG\uart.h
